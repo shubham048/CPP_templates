e03e2c83b9f8b23c6e1d8b7ebfbdc2ac36846442
shubham048 <54982329+shubham048@users.noreply.github.com>
2022-07-03 16:51:29 +0530

    Create ordered_set.cpp

---
+++ ordered_set.cpp
@@ -0,0 +1,10 @@
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os;
+// find_by_order, order_of_key
+//less_equal-multiset
+//greater
