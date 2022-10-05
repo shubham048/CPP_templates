53821694d6dabd2628d55cef4c0b1143a5130b21
shubham048 <shubhamshrm172@gmail.com>
2022-10-05 18:44:01 +0530

    changes segtree

--- segtree.cpp
+++ segtree.cpp
@@ -1,62 +1,85 @@
-struct segtree {
-
-    vector<int> vv;          //change datatype
-    void init(int n) {
-        vv.assign(4 * n, 0);     //change datatype
+struct node {
+    int val;
+    node()
+    {
+        val = 0;
     }
-    int combine(int l, int r) {        // change whole function
-        int res = l + r;
-        return res;
-    }
-    void build(int si, int ss, int se, vector<int> &ar) {
-        if (ss == se) {
-            vv[si] = ar[ss];           //change initialization
-            return;
-        }
-
-        int mid = (ss + se) / 2;
-
-        build(2 * si, ss, mid, ar);
-        build(2 * si + 1, mid + 1, se, ar);
-
-        vv[si] = combine(vv[2 * si], vv[2 * si + 1]);
-    }
-
-    void update(int si, int ss, int se, int qi, int val) {
-        if (ss == se) {
-            vv[si] = val;          //change initialization
-            return;
-        }
-        int mid = (ss + se) / 2;
-
-        if (qi <= mid)update(2 * si, ss, mid, qi, val);
-        else update(2 * si + 1, mid + 1, se, qi, val);
-
-        vv[si] = combine(vv[2 * si], vv[2 * si + 1]);
-    }
-
-    int query(int si, int ss, int se, int qs, int qe) {//change datatype
-        if (qs > se || qe < ss) {
-            return 0;     //change neutral val
-        }
-        if (ss >= qs && qe >= se) {
-            return vv[si];
-        }
-
-        int mid = (ss + se) / 2;
-        int l = query(si * 2, ss, mid, qs, qe); //change datatype
-        int r = query(si * 2 + 1, mid + 1, se, qs, qe);//change datatype
-
-        return combine(l, r);
-    }
-
 };
+class segte {
+public:
+    int n;
+    vector<node> tree;
+    vector<int> a;
+    node neutral;
 
-/* Points to ponder
-    ->change commented lines for different functions
-    ->Keep array 1-indexed
-    ->st.init(n)
-    ->build(1,1,n,array_name);
-    ->query(1,1,n,query_start,query_end);
-    ->update(1,1,n,pos_to_update,val_to_put_at_pos);
-*/
+    void init(int N) {
+        n = N;
+        tree.resize(4 * n + 1);
+        //default values
+        a.assign(n, 0);
+        neutral.val = 0;
+    }
+
+    void put(vector<int> &val) {
+        a = val;
+    }
+
+    //merge function
+    void merge(node &curr, node &left, node &right) {
+        curr.val = max(left.val , right.val);
+    }
+
+    //for leaf
+    void single(node &curr, int idx) {
+        curr.val = a[idx];
+    }
+
+    void build(int index, int ss, int se) {
+        if (ss == se) {
+            single(tree[index], ss);
+            return;
+        }
+        int mid = (ss + se) / 2;
+        build(2 * index, ss, mid);
+        build(2 * index + 1, mid + 1, se);
+        merge(tree[index], tree[2 * index], tree[2 * index + 1]);
+    }
+
+    void build() {
+        build(1, 0, n - 1);
+    }
+
+    node query(int index, int ss, int se, int qs, int qe) {
+        if (qs > se || qe < ss) return neutral;
+        if (qs <= ss && qe >= se) return tree[index];
+        int mid = (ss + se) / 2;
+        node left = query(2 * index, ss, mid, qs, qe);
+        node right = query(2 * index + 1, mid + 1, se, qs, qe);
+        node mer;
+        merge(mer, left, right);
+        return mer;
+    }
+
+    node query(int l, int r) {
+        return query(1, 0, n - 1, l, r);
+    }
+
+    void update(int index, int idx, int ss, int se) {
+        if (idx < ss || idx > se)
+            return;
+        if (ss == se) {
+            single(tree[index], ss);
+            return;
+        }
+        int mid = (ss + se) / 2;
+        update(2 * index, idx, ss, mid);
+        update(2 * index + 1, idx, mid + 1, se);
+        merge(tree[index], tree[2 * index], tree[2 * index + 1]);
+    }
+
+    void update(int idx, int delta) {
+        a[idx] = delta;
+        update(1, idx, 0, n - 1);
+    }
+};
+//0 indexed segtree
