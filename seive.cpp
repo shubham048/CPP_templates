adad7f38dc8158201937a02bfb9bc4a29b27c61a
shubhams84 <115774830+shubhams84@users.noreply.github.com>
2022-10-15 16:11:01 +0530

    Create seive.cpp

---
+++ seive.cpp
@@ -0,0 +1,15 @@
+const int N = 10000000;
+vector<int> lp(N + 1);
+vector<int> pr;
+//if lp[i]=i i is prime else lp[i] is smallest prime divisor
+void seive() {
+    for (int i = 2; i <= N; ++i) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (int j = 0; j < (int)pr.size() && pr[j] <= lp[i] && i * pr[j] <= N; ++j) {
+            lp[i * pr[j]] = pr[j];
+        }
+    }
+}
